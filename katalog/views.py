237961bc124942815a5e676f8f8b3613328d236d
mohamadarvin25 <mohamadarvin84@gmail.com>
2022-09-09 09:50:19 +0700

    berhasil runserver

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    return render(request, "katalog.html",context)
+
+data_catalog_item = CatalogItem.objects.all()
+context = {
+    'list_barang': data_catalog_item,
+    'nama' : 'Mohamad Arvin Fadriasnyah',
+    'id'   : '2006596996'
+
+}
