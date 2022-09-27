197fcaf3ef53a0bf0c9ba39338ddeb5bb6f7072b
mohamadarvin25 <mohamadarvin84@gmail.com>
2022-09-27 21:09:27 +0700

    memasukan konteks kedalam fundction

--- katalog/views.py
+++ katalog/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from katalog.models import CatalogItem
 
 # TODO: Create your views here.
-def show_katalog(request):
-    return render(request, "katalog.html",context)
 
-data_catalog_item = CatalogItem.objects.all()
-context = {
-    'list_barang': data_catalog_item,
-    'nama' : 'Mohamad Arvin Fadriasnyah',
-    'id'   : '2006596996'
-}
+
+def show_katalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_catalog_item,
+        'nama': 'Mohamad Arvin Fadriasnyah',
+        'id': '2006596996'
+    }
+    return render(request, "katalog.html", context)
--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -6,23 +6,29 @@ from django.core import serializers
 # Create your views here.
 from mywatchlist.models import MyWatchListItem
 
-def show_film(request):
-    return render(request, "mywatchlist.html",context)
 
-data_mywatchlist_item = MyWatchListItem.objects.all()
-context = {
-    'list_film': data_mywatchlist_item,
-    'nama' : 'Mohamad Arvin Fadriasnyah',
-    'id'   : '2006596996'
-}
+def show_film(request):
+    data_mywatchlist_item = MyWatchListItem.objects.all()
+    context = {
+        'list_film': data_mywatchlist_item,
+        'nama': 'Mohamad Arvin Fadriasnyah',
+        'id': '2006596996'
+    }
+    return render(request, "mywatchlist.html", context)
+
 
 def show_xml(request):
+    data_mywatchlist_item = MyWatchListItem.objects.all()
     return HttpResponse(serializers.serialize("xml", data_mywatchlist_item), content_type="application/xml")
 
+
 def show_json(request):
+    data_mywatchlist_item = MyWatchListItem.objects.all()
     return HttpResponse(serializers.serialize("json", data_mywatchlist_item), content_type="application/json")
+
 
 data = MyWatchListItem.objects.filter(pk=1)
 
-def show_json_by_id(request,id):
+
+def show_json_by_id(request, id):
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
